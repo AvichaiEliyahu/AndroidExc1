9ee3ae4d8247146b2c0e3013da5cebf1a7c0e994
AvichaiEliyahu <avihayboom@gmail.com>
2020-08-19 13:58:45 +0300

    Home Page - final

--- app/src/main/java/com/example/exc1/MainActivity.java
+++ app/src/main/java/com/example/exc1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.exc1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
         findviews();
         Glide
@@ -27,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
                 .load(R.drawable.main_bgr)
                 .centerCrop()
                 .into(main_IMG_backgroundimg);
+
+        main_BTN_start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                openGameActivity();
+            }
+        });
+    }
+
+    private void openGameActivity() {
+        Intent intent = new Intent(MainActivity.this,activity_game.class);
+        startActivity(intent);
     }
 
     private void findviews() {
