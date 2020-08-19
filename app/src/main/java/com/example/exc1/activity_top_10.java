aaae1cc9461c101ecaf96b9685a61c48fd391612
itay Zemah <itayzemah@gmail.com>
2020-08-19 16:32:05 +0300

    top 10 page

--- app/src/main/java/com/example/exc1/activity_top_10.java
+++ app/src/main/java/com/example/exc1/activity_top_10.java
@@ -9,6 +9,7 @@ public class activity_top_10 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_top_10);
     }
 }
